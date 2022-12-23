b761359cd30dc6f0bda483fee8b12995096390d1
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2022-12-23 18:11:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ import os
 class Config(object):
 
     # Get a bot token from botfather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5945869005:AAHQnHiudJbSMUA-B4znobmcF18DSmxRrE8")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "1954515"))
+    API_HASH = os.environ.get("API_HASH", "9ff4a51876a555bdb530cd6a58baae87")
 
 
     # Array to store users who are authorized to use the bot
